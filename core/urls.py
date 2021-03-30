680022a00e23aa019fe08a264e31de222ecb57c0
Thiagocmwl <costathiago016@gmail.com>
2021-03-30 10:07:57 -0300

    Finished

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import IndexView, PessoaView
+from .views import IndexView, PessoaView, DeleteViewPessoa, CreatePessoa, UpdatePessoa
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('pessoa/<int:pk>', PessoaView.as_view(), name='pessoa')
+    path('pessoa/<int:pk>', PessoaView.as_view(), name='pessoa'),
+    path('<int:pk>', DeleteViewPessoa.as_view(), name='delete'),
+    path('pessoa/criar', CreatePessoa.as_view(), name='create'),
+    path('update/<int:pk>', UpdatePessoa.as_view(), name='update')
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.edit import DeleteView, CreateView, UpdateView
 from .models import Pessoa
 from django.shortcuts import get_object_or_404  
+from django.urls import reverse_lazy
 
 
 class IndexView(ListView):
@@ -17,4 +19,23 @@ class PessoaView(ListView):
 
         return self.pessoa
 
+
+class DeleteViewPessoa(DeleteView):
+    model = Pessoa
+    success_url = reverse_lazy('index')
+
+
+class CreatePessoa(CreateView):
+    model = Pessoa
+    fields = ['nome', 'idade', 'peso', 'genero', 'foto']
+    template_name = 'criar_pessoa.html'
+    success_url = reverse_lazy('index')
+
+
+class UpdatePessoa(UpdateView):
+    model = Pessoa
+    fields = ['nome', 'idade', 'peso', 'genero', 'foto']
+    template_name = 'alterar_pessoa.html'
+    success_url = reverse_lazy('index')
+    template_name_suffix = '_update_form'
 
