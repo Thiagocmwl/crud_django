4cf66599e0b88b890013ef5f04267e3024c062f2
Thiagocmwl <costathiago016@gmail.com>
2021-03-30 07:39:49 -0300

    Hiding secret key

---
+++ crud_django/secret_key.py
@@ -0,0 +1,2 @@
+def get_key():
+    return '^cskr=t^e3s^xgmo99aubxk8!gmxtn&!eps*p8_6oqr-#t5mlj'
--- crud_django/settings.py
+++ crud_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from .secret_key import get_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^cskr=t^e3s^xgmo99aubxk8!gmxtn&!eps*p8_6oqr-#t5mlj'
+SECRET_KEY = get_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
